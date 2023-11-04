757825efbd4bdd36f10b8f8ff54e22efea0d9244
Paraldos <hillbrecht.s@gmail.com>
2023-11-04 23:13:19 +0100

    added basic js functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
-console.log("hi");
+let amountOfSections = 3;
+
+class section {
+  constructor() {
+    this.template = document.querySelector(".section-template");
+    this.section = this.createNewHtmlElement();
+    this.header = this.section.querySelector(".section__header");
+    this.dateInput = this.section.querySelector(".section__date-input");
+    this.addHeaderEvent();
+    this.addDateEvent();
+  }
+
+  createNewHtmlElement() {
+    const fragment = this.template.content.cloneNode(true);
+    document.querySelector("body").appendChild(fragment);
+    const sections = document.querySelectorAll("section");
+    const section = sections[sections.length - 1];
+    return section;
+  }
+
+  addHeaderEvent() {
+    this.header.addEventListener("click", () => {
+      disableAllSections();
+      this.section.classList.remove("disabled");
+    });
+  }
+
+  addDateEvent() {
+    this.dateInput.addEventListener("change", () => {
+      const date = this.dateInput.valueAsNumber;
+      this.header.innerText = new Date(date).toLocaleDateString("de-DE");
+    });
+  }
+}
+
+function init() {
+  for (let i = 0; i < amountOfSections; i++) {
+    new section();
+  }
+  disableAllSections();
+  enableLastSection();
+}
+init();
+
+function disableAllSections() {
+  const sections = document.querySelectorAll("section");
+  sections.forEach((section) => {
+    section.classList.add("disabled");
+  });
+}
+
+function enableLastSection() {
+  const sections = document.querySelectorAll("section");
+  sections[sections.length - 1].classList.remove("disabled");
+}
